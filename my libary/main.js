50b0ede6a5e0efddff65b7be7bb3a51b645a715d
fumbimech <hopeandfavour1@gmail.com>
2024-10-08 13:46:50 -1200

    updated codes

--- my libary/main.js
+++ my libary/main.js
@@ -1,96 +1,89 @@
-const btn = document.getElementById("btn")
-const author = document.getElementById("author")
-const title = document.getElementById("title")
-const pages = document.getElementById("pages")
-const status = document.querySelector(".status")
-const list = document.getElementById("list")
- const host= document.querySelector(".host")
- const hosts = document.querySelector(".hosts")
- const lists = document.getElementById("lists")
- const read = document.getElementById("read")
- const unread = document.getElementById("unread")
- const scope = document.getElementById("scope")
+const btn = document.getElementById("btn");
+const author = document.getElementById("author");
+const title = document.getElementById("title");
+const pages = document.getElementById("pages");
+const status = document.querySelector(".status");
+const list = document.getElementById("list");
+const host = document.querySelector(".host");
+const hosts = document.querySelector(".hosts");
+const lists = document.getElementById("lists");
+const read = document.getElementById("read");
+const unread = document.getElementById("unread");
+const scope = document.getElementById("scope");
+const del = document.getElementById("del");
 let base = document.createElement("base");
- const btn2 = document.querySelector(".btn2")
+let delbtn = document.querySelectorAll(".delbtn")
+let newindex = 0;
+const btn2 = document.querySelector(".btn2");
+const radios = document.querySelectorAll('input[name="ticket')
 
-function Book(author1,title1,pages1,status1) {
-this.author= author1
-this.title = title1
-this.pages = pages1
-this.status= status1
+function Book(author1, title1, pages1, status1) {
+  this.author = author1;
+  this.title = title1;
+  this.pages = pages1;
+  this.status = status1;
 }
-const libary = []; 
-function info(books) {
-  btn.addEventListener("click",() => {
-   if(author.value==='' ) {
-    alert("put in a book")
-  } else {
-        list.innerHTML = "";
-      lists.innerHTML = "";
-      h.innerHTML = "";
-
-const p = document.createElement('p')
-      p.className = "my-para"
-
- function updatestatus(confirm) {
-        if (read.checked) {
-          p.innerHTML = "read";
-        } else if (unread.checked) {
-          p.innerHTML = "not read";
-        } else {
-          p.innerHTML = "";
-        }
-        confirm = p.innerHTML;
-        base = confirm;
-      }
-      updatestatus();
-
-    const pages1 = pages.value
-     const author1 = author.value   
-      const title1 = title.value
-      const status1 = base;
-     const book = new Book(author1,title1,pages1,status1 )
-     libary.push(book)
-
-    books = libary
-
-     function deleteinfo(books,index)  {
-          btn2.addEventListener("click",() => {
-           if(Array.isArray(books) && index >= 0 && index < books.length){
-             books.splice(index,1)
-            } else {
-              console.log("invalid index")
-           }
-           list.innerHTML = "";
-           lists.innerHTML = "";
-           scope.innerHTML = "";
-           h.innerHTML = "";
-        })
-            return books;
-          
-           } 
-           deleteinfo(books,0)
-    for(let i=0; i < books.length; i++) {
-    const li = document.createElement('li')
-    li.className = "my-author"
-     li.innerHTML = books[i].author
-     const titleli = document.createElement('li')
-     titleli.className = "my-title"
-     titleli.innerHTML = books[i].title
-     const pageli = document.createElement('li')
-        pageli.className = "my-page"
-     pageli.innerHTML = books[i].pages
-     h.appendChild(pageli)
-     console.log(titleli)
-     list.appendChild(li) 
-     lists.appendChild(titleli)
-    }   
-    scope.appendChild(p)
-     author.value = ''
-     title .value ='' 
-     pages.value = ''
-     console.log(books)
-     }
-    
-  })}
-info()
+let libary = [];
+     
+function addbook() {
+      const pages1 = pages.value;
+      const author1 = author.value;
+      const title1 = title.value;
+      let selval = ''
+      if (read.checked) {
+        selval = "read";
+     } else  {
+       selval = "not read";
+     } 
+      const status1 = selval;
+      const book = new Book(author1, title1, pages1, status1);
+      libary.push(book);
+}
+ let books;
+ function deletebook (books,index) {
+        books.splice(index,1)
+          list.innerHTML =''
+     lists.innerHTML = ""
+      h.innerHTML = ""
+     scope.innerHTML = ""
+     del.textContent = ""
+            updateinfo()
+ }
+  btn.addEventListener("click", () => {
+          addbook()
+          list.innerHTML =''
+          lists.innerHTML = ""
+         h.innerHTML = ''
+         scope.innerHTML =''
+        del.textContent = ''
+        books = libary
+           updateinfo();
+  })
+      function updateinfo() {
+        for (let i = 0; i < books.length; i++) {
+          const li = document.createElement("li");
+          li.className = "my-author"
+          li.innerHTML = books[i].author;
+          const titleli = document.createElement("li");
+          titleli.className = "my-title"
+          titleli.innerHTML = books[i].title;
+          const pageli = document.createElement("li");
+          pageli.className = "my-page"
+          pageli.innerHTML = books[i].pages;
+          const statusli = document.createElement("li")
+          statusli.innerHTML = books[i].status
+         let deleteli = document.createElement("li");
+           deleteli.textContent = "delete";
+         deleteli.style.backgroundColor = "red";
+       deleteli.className = "my-delete";
+        deleteli.addEventListener("click", () => deletebook(books,i))
+        del.appendChild(deleteli)
+          h.appendChild(pageli);
+           list.appendChild(li);
+           lists.appendChild(titleli);
+           scope.appendChild(statusli);
+          author.value = "";
+          title.value = "";
+          pages.value = "";
+    }}
+ 
